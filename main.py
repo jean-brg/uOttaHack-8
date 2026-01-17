2aa52dc87a755c9adda8894b152ce507d219ec68
ben-rainer <brainer8210@gmail.com>
2026-01-17 05:30:01 -0500

    yay prompt communication :)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # IMPORTS
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
+import os
+import dotenv
+import requests, json
+import time
+
+API_KEY = dotenv.get_key(dotenv_path="./.env", key_to_get="BEN_OPENROUTER")
 
 # APP INIT
 app = Flask(__name__)
@@ -12,9 +18,71 @@ def home():
     return render_template("index.html")
 
 # SAMPLE DATA ROUTE
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    result = {"message": "Connected to Python!"}
+@app.route('/api/prompt', methods=['POST'])
+def execute_prompt():
+    # Save the initial time and JSON arguments
+    init_time = time.time()
+    args = request.get_json()
+
+    # Get the provided arguments
+    prompt = args.get('prompt')
+    model = args.get('model') or "xiaomi/mimo-v2-flash:free"
+    constraints = args.get('constraints')
+
+    response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": "Bearer " + API_KEY
+        },
+        data=json.dumps({
+            "model": model,
+            "messages": [
+                { "role": "system", "content": constraints },
+                { "role": "user", "content": prompt }
+            ],
+            "temperature": 0
+        })
+    )
+
+    # Get the model API's response as a dictionary
+    responsedict = response.json()
+    if(not 'choices' in responsedict.keys()):
+        # If there aren't any responses, it's a fail
+        return jsonify({
+            "start_time": init_time,
+            "end_time": (time.time()),
+            "tokens": 0,
+            "message": responsedict['error']['message'] if ('error' in responsedict.keys()) else 'Something went wrong',
+            "success": False
+        })
+
+    # Show the responses array (we only evaluate the first one though)
+    choices = responsedict['choices']
+
+    # Get the success and message from the API response
+    try:
+        success = ((len(choices) > 0) and ('message' in choices[0].keys()) and ('content' in choices[0]['message'].keys()))
+        if(not success): raise Exception()      # Shortcut to the failiure method if any properties are mising
+        text = choices[0]['message']['content']
+    except Exception:
+        success = False
+        text = 'Something went wrong.'
+    
+    # Return some useful information
+    result = {
+        # Report the model again, just cause
+        "model": model,
+
+        # Metrics
+        "start_time": init_time,
+        "end_time": (time.time()),
+        "tokens": responsedict['usage']['total_tokens'],
+
+        # Prompt response
+        "message": text,
+        "success": success
+    }
+
     return jsonify(result)
 
 # RUNNER
--- static/script.js
+++ static/script.js
@@ -1,9 +1,46 @@
+const models = [
+    "xiaomi/mimo-v2-flash:free",
+    //"openai/gpt-5.2",
+    //"deepseek/deepseek-v3.2",
+    //"google/gemini-3-flash-preview",
+    //"anthropic/claude-sonnet-4.5",
+    "openai/gpt-oss-120b:free",
+    "meta-llama/llama-3.1-405b-instruct:free"
+];
+
+async function promptRequest(prompt, model='xiaomi/mimo-v2-flash:free', constraints='Use only one sentence to respond.') {
+    // Prompt an AI model with a request using the Python backend
+    return await fetch(
+        'http://127.0.0.1:5000/api/prompt',
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                prompt,
+                model,
+                constraints
+            })
+        }
+    ).then(resp => resp.json());
+}
+
 async function getData() {
+    // Stuff to do when button!
     try {
-        const response = await fetch('http://127.0.0.1:5000/api/data');
-        const data = await response.json(); // Convert JSON string to JS object
-        console.log(data.message); // Outputs: "Connected to Python!"
-        document.getElementById('display').innerText = data.message;
+        promptRequest('What is the meaning of life?').then(data => {
+            console.log(data);
+            document.getElementById('display').innerText = data.message;
+        })
+
+        promptRequest('Why is the sky blue?').then(data => {
+            console.log(data);
+            document.getElementById('display').innerText = data.message;
+        })
+
+        promptRequest('2+2', models[1]).then(data => {
+            console.log(data);
+            document.getElementById('display').innerText = data.message;
+        })
     } catch (error) {
         console.error('Error fetching data:', error);
     }
