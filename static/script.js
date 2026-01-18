bd36d859e1e174e3ff7425a5316704f7edefd713
matthewgiithuba <matthewsouza07@gmail.com>
2026-01-18 00:15:41 -0500

    added student class

--- static/script.js
+++ static/script.js
@@ -48,7 +48,6 @@ function switchToClassroom() {
     const classroomImage = document.getElementById("classroom-image");
     const whiteboardSvg = document.getElementById("whiteboard-svg");
     const whiteboardText = document.getElementById("whiteboard-text");
-
     // Start fade overlay
     transition.classList.remove("fade-in");
     transition.classList.add("fade-out");
@@ -66,4 +65,38 @@ function switchToClassroom() {
         transition.classList.remove("fade-out");
         transition.classList.add("fade-in");
     }, 600);
+    
+
+
+}
+let students = [];
+class Student {
+    constructor(eleid, name,top,left,width,imgpath) {
+        this.eleid = eleid;
+        this.name = name;
+        this.img = null; // store the DOM element
+        students.push(this);
+        this.top = top 
+        this.left = left 
+        this.width = width 
+        this.imgPath = imgpath
+    }
+
+    drawOnScreen() {
+        if (this.img) this.img.remove();
+
+        this.img = document.createElement("img");
+        this.img.src = this.imgpath;
+        this.img.id = this.eleid;
+        this.img.style.position = "absolute";
+        this.img.style.top = this.top;
+        this.img.style.left = this.left;
+        this.img.style.width = this.width;
+        this.img.style.height = "auto";
+        this.img.style.imageRendering = "pixelated";
+        this.img.style.zIndex = 10;
+
+        // Add it to the DOM
+        document.body.appendChild(this.imgpath);
+    }
 }
